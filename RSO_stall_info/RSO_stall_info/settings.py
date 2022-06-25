de8f9f6e0209eb7b95dbb75e95988c2b5ffa1b65
UberPipa <nu.takoe.b@gmail.com>
2022-06-25 20:24:02 +0300

    Попытка подключения к бд

--- RSO_stall_info/RSO_stall_info/settings.py
+++ RSO_stall_info/RSO_stall_info/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'RSO_stall_info.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydb',
+        'USER': 'toz',
+        'PASSWORD': '22tatara',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
