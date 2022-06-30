4784f0de04b78bc62329c6990919780d8bee1b55
UberPipa <nu.takoe.b@gmail.com>
2022-06-30 19:49:33 +0300

    Доделываю логирование, нужно ограничить доступ к сайту.

--- RSO_stall_info/main/urls.py
+++ RSO_stall_info/main/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about', views.about, name='about'),
     path('authentication', views.authentication, name='authentication'),
+    path('logout', views.logoutUser, name='logout'),
     path('admin', views.admin, name='admin')
 ]
--- RSO_stall_info/main/views.py
+++ RSO_stall_info/main/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Docs, TypeDocs
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
 
 
 def index(request):
@@ -13,16 +14,22 @@ def about(request):
 
 
 def authentication(request):
-    # if request.method == 'POST':
-    #     form = UserLoginForm(data=request.POST)
-    #     if form.is_valid():
-    #         user = form.get_user()
-    #         login(request, user)
-    #         return redirect('main/index.html')
-    # else:
-    #     form = UserLoginForm()
-    # return render(request, 'main/authentication.html', {'form': form})
-    return render(request, 'main/authentication.html')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.info(request, 'Неверно указан логин или пароль')
+    context = {}
+    return render(request, 'main/authentication.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('authentication')
 
 
 def admin(request):
