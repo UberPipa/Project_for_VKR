256f9e4ae1750f4c3236b9bdf32eb6029ca73efb
UberPipa <nu.takoe.b@gmail.com>
2022-07-17 17:35:26 +0300

    Правлю

--- RSO_stall_info/main/static/main/js/table.js
+++ RSO_stall_info/main/static/main/js/table.js
@@ -1,14 +1,25 @@
 $(document).ready(function() {
     var table = $('#example').DataTable( {
-        lengthChange: true,
-        buttons: [ 'copy',  'print', 'excel', 'pdf', 'colvis'],
+        lengthChange: false,
+        buttons: [
+            'copy',
+            {
+            extend: 'spacer',
+            style: 'bar'
+            },
+            'print',
+            'excel',
+            'pdf',
+            'colvis',
+            'pageLength',
+
+        ],
         select: true,
         autoFill: true,
         colReorder: true,
-        fixedHeader: true
+        fixedHeader: true,
 
     } );
-
     table.buttons().container()
         .appendTo( '#example_wrapper .col-md-6:eq(0)' );
 } );
